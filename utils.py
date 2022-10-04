d28726463bb73c975969bb6ad89e3fb7b226e502
Fourineye <4ineye@gmail.com>
2022-10-04 19:53:16 -0400

    Added raycasting collider and basic object

--- utils.py
+++ utils.py
@@ -1,16 +1,18 @@
 from csv import reader
 from os import walk
 
+import math
 import pygame
 
+Vec2 = pygame.Vector2
 
 def import_csv_layout(path):
-    terrain_map = []
-    with open(path) as level_map:
-        layout = reader(level_map, delimiter=",")
+    data = []
+    with open(path) as data_file:
+        layout = reader(data_file, delimiter=",")
         for row in layout:
-            terrain_map.append(list(row))
-        return terrain_map
+            data.append(list(row))
+        return data
 
 
 def import_folder(path):
@@ -64,7 +66,7 @@ def dist_to(vec1, vec2):
 
 def direction_to(vec1, vec2):
     direction = vec2 - vec1
-    if direction.magnitude():
+    if direction.magnitude() != 1:
         direction.normalize_ip()
     return direction
 
@@ -79,3 +81,44 @@ def key_from_value(search_dict, search_value):
 def scale_double(surface):
     width, height = surface.get_size()
     return pygame.transform.scale(surface, (width * 2, height * 2))
+
+def line_intersection(p_0, p_1, p_2, p_3):
+    '''
+    '''
+    p_0 = pygame.Vector2(p_0)
+    p_1 = pygame.Vector2(p_1)
+    p_2 = pygame.Vector2(p_2)
+    p_3 = pygame.Vector2(p_3)
+    
+    s_1 = pygame.Vector2(p_1 - p_0)
+    s_2 = pygame.Vector2(p_3 - p_2)
+    
+    if abs(s_1.normalize().dot(s_2.normalize())) == 1:
+        return None
+        
+    s = (-s_1.y * (p_0.x - p_2.x) + s_1.x * (p_0.y - p_2.y)) / (-s_2.x * s_1.y + s_1.x * s_2.y)
+    t = ( s_2.x * (p_0.y - p_2.y) - s_2.y * (p_0.x - p_2.x)) / (-s_2.x * s_1.y + s_1.x * s_2.y)
+    
+    if 0 <= s <= 1 and 0 <= t <= 1:
+        i = p_0 + (t * s_1)
+        return pygame.Vector2(i)
+    else:
+        return None
+    
+def line_circle(a, b, c, r):
+    '''
+    '''
+    a = pygame.Vector2(a)
+    b = pygame.Vector2(b)
+    c = pygame.Vector2(c)
+    
+    ac = c - a
+    ab = b - a
+    abab = ab.dot(ab)
+    acab = ac.dot(ab)
+    t = acab / abab
+    h = ab * t + a - c
+    hh = h.dot(h)
+    if hh <= r * r:
+        pen = math.sqrt(r*r - hh)
+        return a + ab * t + pen * direction_to(c + h, a)
