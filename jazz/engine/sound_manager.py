49a5729c2dbd45ae518d07a026142311dd5473a3
Fourineye <4ineye@gmail.com>
2024-08-12 18:22:29 -0400

    Added some music 0layer functionality

--- jazz/engine/sound_manager.py
+++ jazz/engine/sound_manager.py
@@ -12,6 +12,7 @@ class SoundManager:
         self._master_volume = 1.0
         self._volume_m = 1.0
         self._volume_s = 1.0
+        self._music_start = 0
 
     def save_to_ini(self):
         SETTINGS["AUDIO"]["master_volume"] = self._master_volume
@@ -33,6 +34,7 @@ class SoundManager:
     def play_music(self, file=None, loops=0, start=0.0, fade_ms=0):
         if file is not None:
             music.load(file)
+        self._music_start = int(start * 1000)
         music.play(loops, start, fade_ms)
 
     def queue_music(self, file, loops=0):
@@ -40,15 +42,38 @@ class SoundManager:
 
     def stop_music(self):
         music.stop()
+        self._music_start = 0
 
     def pause_music(self):
         music.pause()
 
     def resume_music(self):
         music.unpause()
+        
+    def set_music_pos(self, time: float):
+        """
+            Sets the absolute position of the music playback
+            time: float Position in seconds
+        """
+        status = music.get_busy()
+        #music.pause()
+        if status:
+            self.play_music(start=time)
+        else:
+            music.set_pos(time)
+            self._music_start = int(time * 1000)
+        
+    def get_music_pos(self) -> int:
+        """
+            Returns the time music has been playing in ms
+        """
+        return self._music_start + music.get_pos()
 
     def fadeout_music(self, time):
         music.fadeout(time)
+        
+    def get_music_playing(self) -> bool:
+        return music.get_busy()
 
     def set_music_volume(self, volume):
         self._volume_m = clamp(volume, 0.0, 1.0)
