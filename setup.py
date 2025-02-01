41fbb66adc7906ee98a263854121119b580c82f3
Paul Smith <4ineye@gmail.com>
2025-02-01 14:34:02 -0500

    New Enviroment Setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='jazz',
     version='0.1.1',
-    description='A example Python package',
+    description='A pygame wrapper that provides tools to quickly build games.',
     url='https://github.com/Fourineye/jazz',
     author='Paul Smith',
     author_email='',
